224e5cecabd85e5375eac5640c301654831a4c5f
DESHMUKH, DIPEEKA (dd9280) <dd9280@att.com>
2020-04-16 16:48:07 -0500

    Adding Spring Security to Spring Boot

---
+++ src/main/java/personal/dipeeka/spring_boot_security/App.java
@@ -0,0 +1,13 @@
+package personal.dipeeka.spring_boot_security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/personal/dipeeka/spring_boot_security/HomeResource.java
@@ -0,0 +1,14 @@
+package personal.dipeeka.spring_boot_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String home(){
+		return("<h1>Welcome</h1>");
+	}
+
+}
