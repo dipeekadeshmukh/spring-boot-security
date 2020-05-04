78b131b12a789644e5ca7aaecc530c6183df11a0
DESHMUKH, DIPEEKA (dd9280) <dd9280@att.com>
2020-05-04 16:17:16 -0500

    Added Authentication and Authorization code

--- src/main/java/personal/dipeeka/spring_boot_security/HomeResource.java
+++ src/main/java/personal/dipeeka/spring_boot_security/HomeResource.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 	
+	// should be accessed by all even user is not logged in
 	@GetMapping("/")
 	public String home(){
 		return("<h1>Welcome</h1>");
 	}
+	
+	// should be accessed by all logged in user like user and admin
+	@GetMapping("/user")
+	public String user(){
+		return("<h1>Welcome User</h1>");
+	}
+
+	// should be accessed by all logged in user like admin
+	@GetMapping("/admin")
+	public String admin(){
+		return("<h1>Welcome Admin</h1>");
+	}
+
 
 }
--- src/main/java/personal/dipeeka/spring_boot_security/SecurityConfiguration.java
+++ src/main/java/personal/dipeeka/spring_boot_security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package personal.dipeeka.spring_boot_security;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -42,5 +43,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// the above NoOpPasswordEncoder is basically a PasswordEncoder which does nothing. should not be used in real application
 	}
 	
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//now add the new users and roles to override default behavior
+		http.authorizeRequests()
+		.antMatchers("/**").hasRole("ADMIN")
+		.and().formLogin();
+		// "/**" matches all paths
+	}*/
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//now add the new users and roles to override default behavior
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		// key is to have the more restricted url at the top followed by less restrictive urls. if we put less restrictive at the top, it is going to match all the requests and it will never match more restrictive stuff
+		//.antMatchers("/","static/css","static/js").permitAll();
+		//above line means , user is allowed to access all the static assets like css,js . permit all means user is not w/o authentication
+		.and().formLogin();
 		
+	}
 }
